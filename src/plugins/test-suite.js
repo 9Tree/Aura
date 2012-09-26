7d96cb5712aab4a9a3f2bbec7050b045e9932b80
Carlos Ouro <carlos.ouro@corp.badoo.com>
2012-09-26 17:19:41 +0100

    fatal errors in test do not break the TestCase stack - TestCase still finishes and logs correctly

--- src/plugins/test-suite.js
+++ src/plugins/test-suite.js
@@ -1,20 +1,24 @@
 //test suite
 //no dependencies with Aura itself, so we can test it properly
 TestCase = function(name, f, timeout){
-    this.errors=[];
     this.failed=false;
     this.f = f;
     this.name = name;
     this.timeout = timeout;
     this.run=function(){
         console.log("Start: "+this.name);
-        this.f();
+        try {
+            this.f();
+        } catch(e){
+            this.failed = true;
+            TestSuite.exception(e);
+        }
         if(!this.timeout) this.finish(); //sync
         else {  //async
             var that = this;
             this.timeoutHandler = setTimeout(function(){
                 that.failed = true;
-                that.errors[that.errors.length]="TestCase timed out ("+that.timeout+"ms)";
+                TestSuite.exception("TestCase timed out ("+that.timeout+"ms)");
                 that.finish();
             }, that.timeout);
         }
@@ -22,22 +26,22 @@ TestCase = function(name, f, timeout){
     this.finish=function(){
         if(this.timeoutHandler) clearTimeout(this.timeoutHandler);
         if(this.failed) {
-            for(var i=0;i<this.errors.length;i++){
-                console.log("Failed assert: "+this.errors[i]);
-            }
-            throw "Error! Test failed: "+this.name;
+            TestSuite.exception("Error! Test failed: "+this.name);
         }
-        else console.log("Passed: "+this.name);
+        else TestSuite.log("Passed: "+this.name);
+        TestSuite.dumpLog();
     }
     this.assert=function(what, bool){
         if(!bool) {
             this.failed=true;
-            this.errors[this.errors.length]=what;
+            TestSuite.exception(what);
         }
     }
 }
 TestSuite = {
     tests:{},
+    logStack:[],
+    dumpingLog:false,
     addTest:function(name, f){
         this.tests[name]=new TestCase(name, f);
     },
@@ -53,5 +57,31 @@ TestSuite = {
                 return function(){f.run();}
             })(that.tests[el]),0); //run on "separate threads"
         }
+    },
+    dumpLog:function(){
+        if(this.dumpingLog) return;
+        var that = this;
+        var interval = setInterval(function(){
+            if(that.logStack.length==0) {
+                clearInterval(interval);
+                return;
+            }
+            //avoid problems in the stack
+            setTimeout((function(e){
+                return function(){
+                    if(e.exception){
+                        throw e.value;
+                    } else {
+                        console.log(e.value);
+                    }
+                }
+            })(that.logStack.shift()),0);
+        }, 25);
+    },
+    exception:function(e){
+        this.logStack[this.logStack.length] = {exception:true, value:e}
+    },
+    log:function(e){
+        this.logStack[this.logStack.length] = {exception:false, value:e}
     }
 };
