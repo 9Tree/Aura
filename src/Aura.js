f4362ba3b55d479132a7b02ecbab18ccdf26d5d9
Carlos Ouro <carlos.ouro@9tree.net>
2013-01-02 17:52:28 +0000

    simple aura to the point - object management

--- src/Aura.js
+++ src/Aura.js
@@ -1,284 +1,151 @@
-// $() - DOM Wrapper;       $.func - helper functions;
-// $$() - Object Wrapper;   $$.obj - objects;
-// $$() - Class Wrapper;    $$$.klass - object classes;
+// $$() - Object / Class Wrapper;
 
 //the STUFF
-(function(){
-    "use strict"
+var $$ = (function(){
+    "use strict";
     
-    //window properties setup
-    Object.defineProperties(window, {
-        $:{value:$},
-        $$:{value:$$},
-        $$$:{value:$$$}
-    });
-    
-    //get an id for cache, every Object (including elements)
-    Object.defineProperty(Object.prototype, "__auraId", {
-        value:null,
+    //get an id for cache, every Object
+    Object.defineProperty(Object.prototype, "__aura", {
+        value:undefined,
         writable:true
     });
-    
-    //main DOM selector - very Voodoo!
-    function $(toSelect, scope){
-        scope = scope || window;
-        //cases - TODO: make it logic and faster
-        if (!toSelect) {
-            return new $dom();
-        } else if(toSelect instanceof $dom){
-            return toSelect;
-        } else if (typeof toSelect == 'object') { //Single object or array
-            return new $dom(toSelect);
-        }
-        
-        //ok, string it is
-		toSelect=toSelect.trim();
-        if (toSelect[0] === "#" && toSelect.indexOf(" ") === -1 && toSelect.indexOf(">") === -1) {  //id selector
-            
-            return new $dom(scope.document.getElementById(toSelect.substr(1)));
-              
-        } else if (toSelect[0] === "<" && toSelect[toSelect.length - 1] === ">") {  //html
-            
-            var tmp = document.createElement("div");
-            tmp.innerHTML = toSelect.trim();
-            return new $dom(tmp.children);
-            
-            
-        } else if(toSelect.match(/^\.[a-zA-Z0-9_-]+$/)) { //single class
-            
-            return new $dom(document.getElementsByClassName(c.substr(1)));
-            
-        } else {    //css query selector all
-            
-            var e = [];
-    		try{
-                e = what.querySelectorAll(toSelect);
-    		} catch(e){}
-            return new $dom(slice.call(e));
-            
-        }
-    }
 
     //Object wrapper
     function $$(o){
         //return itself or a new instance
-        return o instanceof $object ? o : new $object(o);
+        var type = typeof o;
+        if(o && (type == 'object' || type == 'function')){
+            return o instanceof $object ? o : new $object(o);
+        }
+        
+        throw new Error("$$ - argument is not a valid javascript object or function");
     }
     
-
-	
 	//
-    //helper functions (+ compatibility replacements)
+	//the main class
 	//
-    
-    //slice
-    var slice = [].slice;
-    
-    //check for a loopable object
-    var canLoop = $.canLoop = function(obj) {return typeof obj == 'object' && obj && typeof obj.length == 'number' }; //Loopable object
-    
-    $.isFunction = function(f){
-        return typeof f === "function";
-    };
-    
-    $.isObject = function(o){
-        return typeof o === "object";
-    };
-    //check number
-    $.isNumeric = function(n){
-		return (n - 0) == n && n.length > 0;
-    };
-    //compare 2 versions
-    $.versionCompare = function(v1,v2){
-        if(v1==v2) return "=";
-        
-        var v1parts = v1.split('.');
-        var v2parts = v2.split('.');
-        //compare the 2 versions
-        for (var i = 0; i < v1parts.length; ++i) {
-            if(v1parts[i] != v2parts[i]){
-                return v1parts[i] > v2parts[i] ? ">" : "<";
-            } else if(v2parts.length == i){
-                return ">";
+    function instanceAura(){
+        if(!o.__aura) {
+            o.__aura={
+                events:{}
             }
         }
-        return "<";
     }
-    //reverse an array
-    $.reverse = function(arr){
-        var n = [];
-        for(var i = arr.length-1; i>=0; i--){
-            n[i+1-arr.length] = arr[i];
-        }
-        return n;
-    };
-    //proxy a method
-	var proxy = $.proxy = function(f, c, args){
-       	return args ? 
-            function(){return f.apply(c, args);} : //use provided arguments
-            function(){return f.apply(c, arguments)} ;	//use scope function call arguments
-	}
-	//check if object is a DOM element
-    $.isElement = function(o){
-        return !!(o && o.nodeType == 1);
-    };
-    //run stuff when DOM is ready
-	$.ready = function(f, scope){
-        scope = scope || window;
-        if (scope.document.readyState === "complete" || scope.document.readyState === "loaded") asap(f);
-        scope.document.addEventListener("DOMContentLoaded", f, false);
-	}
-    var asap = $.asap = function(f){
-        //TODO make this proper and set via postMessage
-        setTimeout(function(){f();},0);
-    }
-    function S4(){
-        return Math.floor(Math.random() * 0x10000).toString(16);
-    }
-    $.uuid = function(){
-        return (
-            S4() + S4() + "-" +
-            S4() + "-" +
-            S4() + "-" +
-            S4() + "-" +
-            S4() + S4() + S4()
-        );
-    }
-    
-    
-    
-    
-    
-	//
-	//the main classes
-	//
-    
-    //common vars
-    var objId=0;
-    
-    
-    //common class methods
-    function getter( i ) {
-        return i == null ? slice.call(this) : ( i < 0 ? this[ this.length + i ] : this[ i ] );
-    }
-    function mapper(f){
-        for(var i=0;i<this.length;i++){
-            var tmp = f.call(this[i], i, this[i]);
-            if(tmp) this[i] = tmp;
-        }
-        return this;
-    }
-    function adder(e){
-        
-    }
-    
-    
+
     //object handler ($$)
     var objectCache={};
-    var $$s = {};
-    function $object(o, id){
-        if(o && typeof o == 'object') {
-            this.length = 0;
-            if(!canLoop(o)) o = [o];
-            for(var i=0;i<o.length;i++) {   //order is important here
-                if(typeof o[i] == 'object'){
-                    if(!o[i].__auraId) {
-                        o[i].__auraId=++objId;
-                        objectCache[objId]={
-                            events:{}
-                        }
-                    }
-                    this[this.length++]=o[i];
-                }
-            }
-            if(id) $$s[id] = this;  //cache this
-            return;
-        } else if(typeof o == 'string') {
-            if($$s[o]) return $$s[o];
-        }
-        throw "$$ - argument is not a valid javascript object or identifier";
+    function $object(o){
+        this.obj = o;
     }
     $object.prototype = {
         //ECMA5 create instance
-        clone:function(p){
-			var id = this[0].__auraId;
-			this[0].__auraId=null;
-			var o = new $object(Object.create(this[0], p));
-			this[0].__auraId=id;
-            return o;
+        copy:function(deep){
+			if(!deep) return this.clone();
+            
+            //TODO - deep object copy
+            var props = this.properties(), i;
+            for()
         },
+        clone:function(p){
+			var aura = this.obj.__aura, o;
+			this.obj.__aura=undefined;
+            o = Object.create(this.obj, p);
+			this.obj.__aura=aura;
+            return $$(o);
+        }
         //ECMA5 inherit properties
         inherit:function(c){
-            this.properties($(c).properties());
+            var aura = this.obj.__aura;
+            this.obj.__aura=undefine;
+            this.propertize($$(c).clone(this.properties()));
+            this.obj.__aura=aura;
+            return this;
         },
         //ECMA5 seed properties
         seed:function(o){
-            
-        }
+            $$(o).inherit(c);
+            return this;
+        },
         bind:function(ev, f){	//bind events to object
-            var objs, obj;
-            ev = Array ? ev : [ev]; //set multiple events at once
-            for(var i=this.length-1;i>=0;i--){
-                objs = objectCache[this[i].__auraId].events;
-                objs = objs || {};
-    			for(var j=this.length-1;j>=0;j--){
-                    obj = objs[ev[j]];
-    				obj = obj || [];
-    				obj.push(f);
-    			}
-            }
+            instanceAura(this.obj);
+            var objs = this.obj.__aura.events, obj, i;
+            
+            ev = Array.isArray(ev) ? ev : [ev]; //set multiple events at once
+            
+			for(i=ev.length-1;i>=0;i--){
+                obj = objs[ev[i]];
+				obj = obj || [];
+				obj.push(f);
+			}
 			return this;
         },
         unbind:function(ev, f){	//unbind events in object
-            var objs, obj;
-            ev = (canLoo(ev) || !ev) ? ev : [ev]; //set multiple events at once
-            for(var i=this.length-1;i>=0;i--){
-                objs = objectCache[this[i].__auraId].events;
-                //clear all events
-                if(!ev || !objs) {
-                    objectCache[this[i].__auraId].events = {};
+            instanceAura(this.obj);
+            var objs = this.obj.__aura.events, obj, i, j;
+            
+            //clear all events
+            if(!ev) {
+                this.obj.__aura.events = {};
+                return;
+            }
+            
+            ev = Array.isArray(ev) ? ev : [ev]; //set multiple events at once
+            
+            //clear matches
+			for(i=ev.length-1;i>=0;i--){
+                obj = objs[ev[i]];
+                
+                //clear all callbacks
+                if(!f || !obj) {
+                    objs[ev[i]] = null;
                     continue;
                 }
-                //clear matches
-    			for(var j=this.length-1;j>=0;j--){
-                    obj = objs[ev[j]];
-                    //clear all callbacks
-                    if(!f || !obj) {
-                        obj = [];
-                        continue;
-                    }
-                    //remove function f
-    				for(var k=0;k<obj.length;k++) if(obj[k]==j) delete obj[k];
-    			}
-            }
+                
+                //remove function f
+				for(j=obj.length-1;j>=0;j--) if(obj[j]==f) delete obj[k];
+			}
+            
+			return this;
+        },
+        trigger:function(ev, args){
+            instanceAura(this.obj);
+            var objs = this.obj.__aura.events, obj, i, j;
+            
+            ev = Array.isArray(ev) ? ev : [ev]; //set multiple events at once
+            
+            //clear matches - loops orders are relevant
+			for(i=0;i<ev.length;i++){
+                obj = objs[ev[i]];
+                
+                //clear all callbacks
+                if(!obj) {
+                    continue;
+                }
+                
+                //fire function f
+				for(j=0;j<obj.length;j++) obj[k].apply(this, args);
+			}
+            
 			return this;
         },
         seal:function(){
-            for(var i=this.length-1;i>=0;i--) 
-                Object.seal(this[i]);
+            Object.seal(this.obj);
         },
         freeze:function(){
-            for(var i=this.length-1;i>=0;i--) 
-                Object.freeze(this[i]);
+            instanceAura(this.obj); //otherwise events can not work after freeze
+            Object.freeze(this.obj); 
         },
 		delegate:function(){	//delegate events to another object
 			//TODO
 			return this;
 		},
-        // "kills" the $$ object
-        kill:function(){
-            for(var i=this.length-1;i>=0;i--) {
-                delete objectCache[this[i].__auraId];
-                delete this[i];
-            }
-        },
 		stringify:function(){	//stringify object FIXME
-            if(!this[0]) return null;
+            if(!this.obj) return null;
 			//remove aura stuff to stringify
-			var id = this[0].__auraId;
-			this[0].__auraId=null;
-			var str = JSON.stringify(this[0]);
-			this[0].__auraId=id;
+			var aura = this.obj.__aura;
+			this.obj.__aura=undefined;
+			var str = JSON.stringify(this.obj);
+			this.obj.__aura=aura;
 			return str;
 		},
 		querystring:function(urlEncode){	//convert to http GET/POST querystring
@@ -286,195 +153,45 @@
 		},
         property:function(n, p){  //ECMA 5 properties
             if(p){
-                for(var i=this.length-1;i>=0;i--) 
-                    Object.defineProperty(this[i], n, p);
+                Object.defineProperty(this.obj, n, p);
                 return this;
             } else {
-                return Object.getOwnPropertyDescriptor(this[0], n);
+                return Object.getOwnPropertyDescriptor(this.obj, n);
             }
         },
         properties:function(p){
             if(p){  //add properties
-                for(var i=this.length-1;i>=0;i--) 
-                    for(var el in p) 
-                        Object.defineProperty(this[i], el, p[el]);
+                for(var el in p) 
+                    Object.defineProperty(this.obj, el, p[el]);
                 return this;
             } else {
-                var props = Object.getOwnPropertyNames(this[0]);
+                var props = Object.getOwnPropertyNames(this.obj);
                 var result = {};
                 for(var i=props.length-1;i>=0;i--){   //order is irrelevant for the return
-                    result[props[i]] = Object.getOwnPropertyDescriptor(this[0], props[i]);
+                    result[props[i]] = Object.getOwnPropertyDescriptor(this.obj, props[i]);
                 }
                 return result;
             }
         },
-        setProperties:function(p){   //replace properties
-          for(var i=this.length-1;i>=0;i--) 
-              Object.defineProperties(this[i], p); 
+        propertize:function(p){   //replace properties
+          Object.defineProperties(this.obj, p); 
           return this; 
         },
-        //common methods
-        get:getter,
-        map:mapper
+        get:function() {
+            return this.obj;
+        }
+        
+        //function only methods
     }
     
-    //single DOM object class ($)
-    function $dom(e, scope){
-        this.scope = scope;
-        this.length = 0;
-        if(e){
-            if(!e.nodeType && canLoop(e)) {
-                for(var i=0;i<e.length;i++) this[this.length++]=e[i]; //order matters
-            } else {
-                this[0]=e;
-                this.length=1;
-            }  
-        }
-    }
-    $dom.prototype = {
-        attr : function(a, v){
-            if(!v){
-                return this[0][a];
-            } else {
-                for(var i=this.length-1;i>=0;i--) this[i].setAttribute(a, v);
-				return this;
-            }
-        },
-        removeAttr : function(a){
-            for(var i=this.length-1;i>=0;i--) this[i].removeAttribute(a);
-            return this;
-        },
-        data : function(a,v){return this.attr('data-'+a,v);},    //still 3x faster than dataset
-        
-        find : function(toSelect){
-            //TODO - review
-            if (!this[0])
-                return undefined;
-                
-            if (typeof what == 'object') { //var tmp=$("span");  $("p").find(tmp);
-                if (toSelect.length == undefined) {
-                    if (toSelect.parentNode == what)
-                        return new $dom(toSelect);
-                    else return new $dom();
-                } else {
-                    var a = [];
-                    for (var i = 0; i < toSelect.length; i++){
-                        if (toSelect[i].parentNode == what)
-                            a[a.length] = toSelect[i];
-                    }
-                    return new $dom(a);
-                }
-            }
-            
-            //ok, selector it is
-    		toSelect=toSelect.trim();
-            if (toSelect[0] === "#" && toSelect.indexOf(" ") === -1 && toSelect.indexOf(">") === -1) {  //id selector
-                
-        		try{
-        			return new $dom(this[0].querySelector(toSelect));
-        		} catch(e){}
-                
-            } else {    //css query selector all
-                
-        		try{
-                    return new $dom(this[0].querySelectorAll(toSelect));
-        		} catch(e){}
-                
-            }
-            return new $dom();
-        },
-        val : function(v){
-            if(v!==undefined){  //setter
-                
-                for(var i=this.length-1;i>=0;i--){
-                    //special select case
-                    if(this[i].tagName=="SELECT"){
-                        var els = this[i].find("option");   //get options
-                        v = canLoop(v) ? v : [v];   //
-                        //set selected
-                        for(var i=els.length-1;i>=0;i--){
-                            els[j].selected = v.indexOf(els[j].value)!=-1;
-                        }
-                    } else this[i].value = v;
-                }
-                return this;
-                
-            } else if(this[0]){ //getter
-                
-                if(this[0].tagName=="SELECT"){
-                    //special select case
-                    //TODO improve to ignore disabled optgroups
-                    return this.find('option[selected]:not([disabled])').val();
-                } else if (this.length>1){
-                    //array case
-                    var values = [];
-                    for(var i=0;i<this.length;i++){
-                        //special select case
-                        values.push(this[i].value);
-                    }
-                    return values;
-                } else {
-                    //single value
-                    return this[0].value;
-                }
-                
-            } else return null;
-        },
-        addClass:function(c){
-            c = c.split(' ');
-            for(var i=this.length-1;i>=0;i--) {
-                for(var j=c.length-1;j>=0;j--) this[i].classList.add(c[j]); 
-            } 
-            return this;
-        },
-        hasClass:function(c){
-            return this.length>0 ? this[0].classList.contains(c); : false;
-        },
-        removeClass:function(c){
-            c = c.split(' ');
-            for(var i=this.length-1;i>=0;i--) 
-                for(var j=c.length-1;j>=0;j--) 
-                    this[i].classList.remove(c[j]);
-            return this;
-        },
-        replaceClass:function(cName, newClass){
-            cName = cName.split(' ');
-            newClass = newClass.split(' ');
-            for (var i=this.length-1;i>=0;i--) {
-                for(var j=cName.length-1;j>=0;j--) this[i].classList.remove(cName[j]);
-                for(var j=newClass.length-1;j>=0;j--) this[i].classList.add(newClass[j]);
-            }
-            return this;
-        },
-        css: function( name, value ) {
-            //setter
-            if(value || typeof name == 'object') return this.style(name, value);
-            //getter (computedStyle property)
-            if(name) return this.scope.getComputedStyle(this[0])[name];
-            //getter (full computedStyle)
-            return this.scope.getComputedStyle(this[0]);
-        },
-        style: function(name, value){
-            if(value) name = {name:value};
-            
-            if(typeof name == 'object'){    //setter
-                for(var i=this.length-1;i>=0;i--)
-                    for(el in name) this[i].style[el]=name[el];
-                return this;
-            } else {    //getter
-                return this[0].style[name];
-            }
-        },
-        show: function() {this.css('display', '')},
-        hide: function() {},
-        toggle: function() {
-            for(var i=this.length-1;i>=0;i--){
-                //if(this[i].style.display)
-            }
-        },
-        
-        //common methods
-        get:getter,
-        map:mapper
-    }
-})()
+    
+})();
+
+if(window){
+    //window properties setup
+    Object.defineProperties(window, {
+        $$:{value:$$}
+    });
+    //delete from current scope
+    delete $$;
+}
--- tests/tests.js
+++ tests/tests.js
@@ -1,134 +1,79 @@
-TestSuite.addTest("$$$ inheritance", function(){
-    //$$$ inheritance tests
-    //class a
-    function a(test){
-        this.check = "a";
-    };
-    //setup a's prototype
-    $$$(a).proto({
-        hello:null, 
-        bye:function(){
-            return "bye";
-        }
-    });
-    //p prototype object
-    var p = {
-        hello:function(){
-            return "hello";
-        }
-    };
-    //class f
-    var f = function(){
-        this.check = "f";
+function assert(what, val){
+    if(!val) throw(what);
+}
+
+
+
+//simplest tests as can be
+
+//$$$ inheritance tests
+//class a
+var a = { check : "a" };
+
+//setup a's prototype
+$$(a).properties({
+    hello:null, 
+    bye:function(){
+        return "bye";
     }
-    //prototype f with p
-    $$$(f).proto(p);
-    //b will be p extending a + property test
-    var b = $$$(a).seed(p).property("test", {
-        value:"test"
-    });
-    //c will be f extending b + property test2
-    var c = $$$(b).seed(f).properties({
-        test2:{
-            configurable:false, 
-            get:function(){return this.value;},
-            set:function(v){this.value='set:'+v;}
-        }
-    });
-    //g will be p extending c + property test2
-    var g = $$$(c).seed({}).properties({
-        hello:{
-            value:"hello2"
-        }
-    });
+});
+//p prototype object
+var p = {
+    hello:function(){
+        return "hello";
+    }
+};
+//class f
+var a = { check = "f" };
+
+//prototype f with p
+$$(f).properties(p);
+
+//b will be p extending a + property test
+var b = $$(a).seed(p).property("test", {
+    value:"test"
+});
+
+//c will be f extending b + property test2
+var c = $$(b).seed(f).properties({
+    test2:{
+        configurable:false, 
+        get:function(){return this.value;},
+        set:function(v){this.value='set:'+v;}
+    }
+});
+
+//g will be p extending c + property test2
+var g = $$(c).clone({
+    hello:{
+        value:"hello2"
+    }
+});
 
 
-    var d = b.new("Start test!");
-    this.assert("Not an instance of a", !(d instanceof a));
-    this.assert("d.check", d.check=="a");
-    this.assert("d.hello()", d.hello()=="hello");
-    this.assert("d.test", d.test=="test");
-    this.assert("d.bye()", d.bye()=="bye");
+var d = b.new("Start test!");
+assert("Not an instance of a", !(d instanceof a));
+assert("d.check", d.check=="a");
+assert("d.hello()", d.hello()=="hello");
+assert("d.test", d.test=="test");
+assert("d.bye()", d.bye()=="bye");
 
-    var e = c.new("test2");
-    this.assert("e an instance of f", e instanceof f);
-    this.assert("e.check", e.check=="f");
-    this.assert("e.hello()", e.hello()=="hello");
-    this.assert("e.test", e.test=="test");
-    e.test2 = "test2";
-    this.assert("e.test2", e.test2=="set:test2");
-    this.assert("e.bye()", e.bye()=="bye");
+var e = c.new("test2");
+assert("e an instance of f", e instanceof f);
+assert("e.check", e.check=="f");
+assert("e.hello()", e.hello()=="hello");
+assert("e.test", e.test=="test");
+
+e.test2 = "test2";
+assert("e.test2", e.test2=="set:test2");
+assert("e.bye()", e.bye()=="bye");
     
-    var h = g.new("test2");
-    this.assert("h not an instance of f", !(h instanceof f));
-    this.assert("h.check", h.check=="f");
-    this.assert("h.hello", h.hello=="hello2");
-    this.assert("h.test", h.test=="test");
-    h.test2 = "test2";
-    this.assert("h.test2", h.test2=="set:test2");
-    this.assert("h.bye()", h.bye()=="bye");
-});
+var h = g.new("test2");
+assert("h not an instance of f", !(h instanceof f));
+assert("h.check", h.check=="f");
+assert("h.hello", h.hello=="hello2");
+assert("h.test", h.test=="test");
 
-
-TestSuite.addAsyncTest("$.ready", function(){
-    var that = this;
-
-    $.ready(function(){
-        that.finish();
-    });
-});
-
-
-
-TestSuite.runAll(true);
-
-// var __color = {red:0,green:0,blue:0,alpha:1};
-// function createGetMethod(prop){
-//     return function(){return __color[prop]};
-// }
-// function createSetMethod(prop){
-//     return function(val){__color[prop]=val};
-// }
-// function createDescritorProperty(prop){
-//     return {
-//         get: createGetMethod(prop),
-//         set: createSetMethod(prop)
-//     }
-// }
-// 
-// var _color_descriptor = {
-//     red:createDescritorProperty('red'),
-//     green:createDescritorProperty('green'),
-//     blue:createDescritorProperty('blue'),
-//     alpha:createDescritorProperty('alpha'),
-//     length:{
-//         value:4,
-//         writable:false,
-//         enumerable : true
-//     }
-// }
-// //setup numbered values
-// _color_descriptor[0]=createDescritorProperty('red');
-// _color_descriptor[1]=createDescritorProperty('green');
-// _color_descriptor[2]=createDescritorProperty('blue');
-// _color_descriptor[3]=createDescritorProperty('alpha');
-// 
-// var _color = Object.create(Array.prototype, _color_descriptor); //extends array
-// 
-// 
-// Object.defineProperty(window, 'color', {
-//     get: function() {
-//         return _color;
-//     },
-//     set: function(val) {
-//         _color.red = val[0];
-//         _color.green = val[1];
-//         _color.blue = val[2];
-//         _color.alpha = val[3];
-//     }
-// });
-// 
-// window.color = [255,255,255,0];
-// alert(window.color);
-// window.color.red=10;
-// alert(window.color);
+h.test2 = "test2";
+assert("h.test2", h.test2=="set:test2");
+assert("h.bye()", h.bye()=="bye");
